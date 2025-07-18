b37d4783bf9a82079d772eb3e592b00cd976d518
Chika1703 <dimakolb2@.gmail.com>
2025-07-18 09:55:22 +0300

    Revert "zalupa"
    This reverts commit 6c3ea8fe9b2df4a3eeb2ec139e4a129aac14f8f1.

--- src/main/java/Welcomer.java
+++ src/main/java/Welcomer.java
@@ -1,21 +1,9 @@
 public class Welcomer {
-	public String sayWelcome() {
-		return "Welcome home, good hunter. What is it your desire?";
-	}
+    public String getWelcomeMessage() {
+        return "Welcome to our application!";
+    }
 
-	public String sayFarewell() {
-		return "Farewell, good hunter. May you find your worth in waking world.";
-	}
-
-	public String sayNeedGold() {
-		return "Not enough gold";
-	}
-
-	public String saySome() {
-		return "something in the way";
-	}
-
-	public String sayCasador() {
-		return "Casador means hunter.";
-	}
+    public String getHunterMessage() {
+        return "The hunter is on the prowl!";
+    }
 }
--- src/test/java/WelcomerTest.java
+++ src/test/java/WelcomerTest.java
@@ -1,40 +1,12 @@
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.Assert.*;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WelcomerTest {
 
-	private Welcomer welcomer = new Welcomer();
-
-	@Test
-	public void welcomerSaysWelcome() {
-		assertThat(welcomer.sayWelcome(), containsString("Welcome"));
-	}
-
-	@Test
-	public void welcomerSaysFarewell() {
-		assertThat(welcomer.sayFarewell(), containsString("Farewell"));
-	}
-
-	@Test
-	public void welcomerSaysHunter() {
-		assertThat(welcomer.sayWelcome(), containsString("hunter"));
-		assertThat(welcomer.sayFarewell(), containsString("hunter"));
-	}
-
-	@Test
-	public void welcomerSaysSilver() {
-		assertThat(welcomer.sayNeedGold(), containsString("gold"));
-	}
-
-	@Test
-	public void welcomerSaysSomething() {
-		assertThat(welcomer.saySome(), containsString("something"));
-	}
-
-	@Test
-	public void welcomerSayCasador() {
-		assertThat(welcomer.sayCasador(), containsString("hunter"));
-	}
+    @Test
+    public void testGetHunterMessage() {
+        Welcomer welcomer = new Welcomer();
+        String message = welcomer.getHunterMessage();
+        assertTrue(message.contains("hunter"), "Message should contain 'hunter'");
+    }
 }
