606fa74283ee412f8ad2032c1fb98c644ab37608
Chika1703 <dimakolb2@.gmail.com>
2025-07-18 09:55:31 +0300

    Revert "Reapply "zalupa""
    This reverts commit 137c8f750a9770b2a1ef407836b52ca362440169.

--- src/main/java/HelloPlayer.java
+++
@@ -1,7 +0,0 @@
-public class HelloPlayer{
-	public static void main(String[] args) {
-		Welcomer welcomer = new Welcomer();
-		System.out.println(welcomer.sayWelcome());
-		System.out.println(welcomer.sayFarewell());
-	}
-}
